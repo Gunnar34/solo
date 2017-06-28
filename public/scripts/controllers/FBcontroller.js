e7ad4f4c0d78c1d1696f02d3c1d7371455a6de15
Noah Rolf <noahrolf1@gmail.com>
2017-06-28 09:24:25 -0500

    added a scond controller

---
+++ public/scripts/controllers/FBcontroller.js
@@ -0,0 +1,54 @@
+app.controller('facebookController', facebookController);
+
+  function facebookController(FbService, locService){
+    var vm =this;
+    var lat;
+    var lon;
+    vm.events = [];
+    vm.showMenu = false;
+
+    vm.init = function(){
+      FbService.getAccess();
+      navigator.geolocation.getCurrentPosition(findLocation);
+    }; //pageload function
+
+    vm.init();
+
+    vm.togglePannel = function(index){
+      vm.events[index].eventPannel = !vm.events[index].eventPannel;
+    }; //flip divs for facebook events
+
+    function findLocation(position) {
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+      console.log(lat, lon);
+      vm.getEvents();
+    } //find current location
+
+
+    vm.getEvents = function (){
+      var time = (Math.floor(Date.now() / 1000)) + 172800;
+      console.log('unix time: ', time);
+      if (lat === undefined || lon === undefined) {
+        lat = 44.986656;
+        lon = -93.258133;
+        }
+      FbService.getEvents(time, lat, lon).then(function(res){
+        vm.events = res;
+        console.log(res);
+        });
+    }; //get facebook events with service and API get
+
+    vm.getLocation = function(){
+      locService.getKey().then(function(res){
+        var address = vm.location;
+        vm.location = '';
+        locService.getLocation(address).then(function(res){
+          lat = res.lat;
+          lon = res.lng;
+          }).then(vm.getEvents());
+      }); //get addrress to lon lat with google api, then events
+
+    };
+
+  }
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,63 +6,18 @@ app.config(function ($routeProvider){
   }).when('/gettingStarted', {
     templateUrl: 'view/gettingStarted.html'
   }).when('/facebook', {
-    templateUrl: 'view/facebook.html'
+    templateUrl: 'view/facebook.html',
+    controller: 'facebookController as fb'
   });
 });
 
 app.controller('bodyController', bodyController);
 
-function bodyController(FbService, locService){
+function bodyController(){
   var vm = this;
-  var lat;
-  var lon;
-  vm.events = [];
-  vm.showMenu = false;
-
-  vm.init = function(){
-    FbService.getAccess();
-    navigator.geolocation.getCurrentPosition(findLocation);
-  }; //pageload function
-
-  vm.togglePannel = function(index){
-    vm.events[index].eventPannel = !vm.events[index].eventPannel;
-  }; //flip divs for facebook events
 
   vm.displayMenu = function(){
-    vm.showMenu = !vm.showMenu;
-  }; //hide show menu
-
-  function findLocation(position) {
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-    console.log(lat, lon);
-    vm.getEvents();
-  } //find current location
-
-
-  vm.getEvents = function (){
-    var time = (Math.floor(Date.now() / 1000)) + 172800;
-    console.log('unix time: ', time);
-    if (lat === undefined || lon === undefined) {
-      lat = 44.986656;
-      lon = -93.258133;
-      }
-    FbService.getEvents(time, lat, lon).then(function(res){
-      vm.events = res;
-      console.log(res);
-      });
-  }; //get facebook events with service and API get
-
-  vm.getLocation = function(){
-    locService.getKey().then(function(res){
-      var address = vm.location;
-      vm.location = '';
-      locService.getLocation(address).then(function(res){
-        lat = res.lat;
-        lon = res.lng;
-        }).then(vm.getEvents());
-    }); //get addrress to lon lat with google api, then events
-
-  };
+      vm.showMenu = !vm.showMenu;
+    }; //hide show menu
 
 }
