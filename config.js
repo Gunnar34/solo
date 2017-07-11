f79a3d4874a55039309b6cedaa78013c232c6806
Noah Rolf <noahrolf1@gmail.com>
2017-07-11 17:12:18 -0500

    heroku

---
+++ config.js
@@ -0,0 +1,8 @@
+var data = {
+  app_id: '133127177264282',
+  app_secret: '26d59c41b0ac2d9e25c51841a31cee3d',
+  gmaps: 'AIzaSyDkOxLuMR4oPJ_3UYb_40xr3ZsM4melZz4',
+  places: 'AIzaSyAO_ZYVjCA7oxtW2S9lfhNT5RI-I-f2FWc'
+};
+
+module.exports = data;
