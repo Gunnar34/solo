979989c30d484e68e81529de6cf99691f458b7ab
Noah Rolf <noahrolf1@gmail.com>
2017-07-12 16:24:54 -0500

    date fix

--- public/scripts/services/FbService.js
+++ public/scripts/services/FbService.js
@@ -23,12 +23,11 @@ app.service('FbService', function($http) {
 
     return $http.get(url).then(function(res){
       var data = [];
-      console.log('before loop', res.data.events);
       for (var i = 0; i < res.data.events.length; i++) {
-        res.data.events[i].startTimeDisplay = new Date(res.data.events[i].startTime);
-        res.data.events[i].startTimeDisplay = (dateFormat(res.data.events[i].startTimeDisplay, "dddd, mmmm dS, yyyy, h:MM TT"));
-        res.data.events[i].endTimeDisplay = new Date(res.data.events[i].endTime);
-        res.data.events[i].endTimeDisplay = (dateFormat(res.data.events[i].endTimeDisplay, "dddd, mmmm dS, yyyy, h:MM TT"));
+        // res.data.events[i].startTimeDisplay = new Date(res.data.events[i].startTime);
+        // res.data.events[i].startTimeDisplay = (dateFormat(res.data.events[i].startTimeDisplay, "dddd, mmmm dS, yyyy, h:MM TT"));
+        // res.data.events[i].endTimeDisplay = new Date(res.data.events[i].endTime);
+        // res.data.events[i].endTimeDisplay = (dateFormat(res.data.events[i].endTimeDisplay, "dddd, mmmm dS, yyyy, h:MM TT"));
         if (res.data.events[i].category === null) {
           res.data.events[i].category = 'unknown';
         }
@@ -40,7 +39,6 @@ app.service('FbService', function($http) {
         data.push(res.data.events[i]);
       }
       console.log('after loop', data);
-      console.log(res.data.events[0].startTime);
       return (data);
     });
   };
